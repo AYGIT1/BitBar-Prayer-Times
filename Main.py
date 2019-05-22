81442043fb4eaec6a2ffbd9d88712bd1d13601f6
AYGIT1 <AYGIT1@users.noreply.github.com>
2019-05-23 01:26:43 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,34 @@
+import requests
+
+
+Aranan_ulke = "ulke"
+Aranan_sehir = "sehir"
+Aranan_ilce = "ilce"
+
+
+def ulke_sec():
+    ulkeler = requests.get("http://ezanvakti.herokuapp.com/ulkeler")
+    for
+    return ulke_id
+
+
+def sehir_sec(ulke_id):
+    foo
+    return
+
+
+def ilce_sec():
+    foo
+    return
+
+
+def ezan_saat_cek():
+    return
+
+
+def dosyaya_yaz():
+    return
+
+
+
+print("test")
