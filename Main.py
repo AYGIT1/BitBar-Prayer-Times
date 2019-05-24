a35ad05faaaf2f617563eb22dbf8e015541b78f4
AYGIT1 <AYGIT1@users.noreply.github.com>
2019-05-24 19:17:20 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,46 @@
 import requests
 
 
-Aranan_ulke = "ulke"
-Aranan_sehir = "sehir"
-Aranan_ilce = "ilce"
+Country_to_search = "TURKEY"        #Ülke
+Province_to_search = "ANKARA"       #Şehir
+District_to_search = "cankaya"      #İlçe
 
 
-def ulke_sec():
-    ulkeler = requests.get("http://ezanvakti.herokuapp.com/ulkeler")
-    for
-    return ulke_id
+def find_country_id():
+    countries = requests.get("http://ezanvakti.herokuapp.com/ulkeler")
+    for data in countries.json():
+        if data["UlkeAdiEn"] == Country_to_search.upper():
+            print(data)
+            return data["UlkeID"]
 
 
-def sehir_sec(ulke_id):
-    foo
+def find_province_id(ID):
+    url = "http://ezanvakti.herokuapp.com/sehirler?ulke=" + ID
+    provinces = requests.get(url)
+    for data in provinces.json():
+        if data["SehirAdiEn"] == Province_to_search.upper():
+            print(data)
+            return data["SehirID"]
+
+
+def find_district_id(ID):
+    url = "http://ezanvakti.herokuapp.com/ilceler?sehir=" + ID
+    districts = requests.get(url)
+    for data in districts.json():
+        if data["IlceAdiEn"] == Province_to_search.upper():
+            print(data)
+            return data["IlceID"]
+
+
+def pull_prayer_times(ID):
     return
 
 
-def ilce_sec():
-    foo
-    return
-
-
-def ezan_saat_cek():
-    return
-
-
-def dosyaya_yaz():
+def write_to_file():
+    file = open("test.ezan", mode='w', encoding='utf-8')
     return
 
 
 
 print("test")
+find_district_id(find_province_id(find_country_id()))
