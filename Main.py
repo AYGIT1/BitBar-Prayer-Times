025a653edf7b5c98b26a9304fbe69829da8973df
AYGIT1 <AYGIT1@users.noreply.github.com>
2019-05-25 00:13:11 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,39 +1,36 @@
 import requests
 
+Country_to_search = "TURKEY"  # Ülke
+Province_to_search = "ANKARA"  # Şehir
+District_to_search = "cankaya"  # İlçe
 
-Country_to_search = "TURKEY"        #Ülke
-Province_to_search = "ANKARA"       #Şehir
-District_to_search = "cankaya"      #İlçe
 
-
-def find_country_id():
+def get_prayer_times():
+    # Find country ID
     countries = requests.get("http://ezanvakti.herokuapp.com/ulkeler")
-    for data in countries.json():
-        if data["UlkeAdiEn"] == Country_to_search.upper():
-            print(data)
-            return data["UlkeID"]
+    for data_country in countries.json():
+        if data_country["UlkeAdiEn"] == Country_to_search.upper():
+            country_id = data_country["UlkeID"]
 
+    # Find province ID
+    url_province = "http://ezanvakti.herokuapp.com/sehirler?ulke=" + country_id
+    provinces = requests.get(url_province)
+    for data_province in provinces.json():
+        if data_province["SehirAdiEn"] == Province_to_search.upper():
+            province_id = data_province["SehirID"]
 
-def find_province_id(ID):
-    url = "http://ezanvakti.herokuapp.com/sehirler?ulke=" + ID
-    provinces = requests.get(url)
-    for data in provinces.json():
-        if data["SehirAdiEn"] == Province_to_search.upper():
-            print(data)
-            return data["SehirID"]
+    # Find district ID
+    url_district = "http://ezanvakti.herokuapp.com/ilceler?sehir=" + province_id
+    districts = requests.get(url_district)
+    for data_district in districts.json():
+        if data_district["IlceAdiEn"] == Province_to_search.upper():
+            print(data_district)
+            district_id = data_district["IlceID"]
 
-
-def find_district_id(ID):
-    url = "http://ezanvakti.herokuapp.com/ilceler?sehir=" + ID
-    districts = requests.get(url)
-    for data in districts.json():
-        if data["IlceAdiEn"] == Province_to_search.upper():
-            print(data)
-            return data["IlceID"]
-
-
-def pull_prayer_times(ID):
-    return
+    url_prayer = "http://ezanvakti.herokuapp.com/vakitler?ilce=" + district_id
+    prayer = requests.get(url_prayer)
+    print(prayer.json())
+    return prayer.json()
 
 
 def write_to_file():
@@ -41,6 +38,5 @@ def write_to_file():
     return
 
 
-
 print("test")
-find_district_id(find_province_id(find_country_id()))
+get_prayer_times()
