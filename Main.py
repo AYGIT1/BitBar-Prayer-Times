619c7a09c02903bd2e351c05fcc434e752020555
İbrahim Ethem Demirci <dmrc_wr@hotmail.com>
2019-05-25 13:27:47 +0300

    minor performance changes and code quality improvements

--- Main.py
+++ Main.py
@@ -3,45 +3,39 @@ import json
 
 # TODO: Exception handling
 
-
 def get_prayer_times():
 
-    Country_to_search = "TURKEY"  # Ülke
-    Province_to_search = "ANKARA"  # Şehir
-    District_to_search = "ANKARA"  # İlçe
+    country = "TURKEY"  # Ülke
+    province = "ANKARA"  # Şehir
+    district = "ANKARA"  # İlçe
+    url = "https://ezanvakti.herokuapp.com"
 
     # Find country ID
-    countries = requests.get("http://ezanvakti.herokuapp.com/ulkeler")
+    countries = requests.get(url + "/ulkeler")
     for data_country in countries.json():
-        if data_country["UlkeAdiEn"] == Country_to_search.upper():
+        if data_country["UlkeAdiEn"] == country.upper():
             country_id = data_country["UlkeID"]
 
     # Find province ID
-    url_province = "http://ezanvakti.herokuapp.com/sehirler?ulke=" + country_id
-    provinces = requests.get(url_province)
+    provinces = requests.get(url + f"/sehirler?ulke={country_id}")
     for data_province in provinces.json():
-        if data_province["SehirAdiEn"] == Province_to_search.upper():
+        if data_province["SehirAdiEn"] == province.upper():
             province_id = data_province["SehirID"]
 
     # Find district ID
-    url_district = "http://ezanvakti.herokuapp.com/ilceler?sehir=" + province_id
-    districts = requests.get(url_district)
+    districts = requests.get(url + f"/ilceler?sehir={province_id}")
     for data_district in districts.json():
-        if data_district["IlceAdiEn"] == District_to_search.upper():
+        if data_district["IlceAdiEn"] == district.upper():
             district_id = data_district["IlceID"]
 
     # Get prayer times for the district
-    url_prayer = "http://ezanvakti.herokuapp.com/vakitler?ilce=" + district_id
-    prayer = requests.get(url_prayer)
+    prayer = requests.get(url + f"/vakitler?ilce={district_id}")
     return prayer.json()
 
 
 def write_to_file(data):
-    file = open("test.ezan", mode='w', encoding='utf-8')
-    for element in data:
-        file.write(json.dumps(element) + "/n")
-    file.close()
-    return
+    with open('.ptimes.json', 'w') as json_file:
+        json.dump(data, json_file)
 
 
 write_to_file(get_prayer_times())
