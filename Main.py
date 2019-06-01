7c135fcc1e5ed24504c000913e10295763fc424d
AYGIT1 <AYGIT1@users.noreply.github.com>
2019-06-01 07:26:17 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,62 +7,55 @@ import datetime
 
 def get_prayer_times():
 
-    Country_to_search = "TURKEY"  # Ülke
-    Province_to_search = "ANKARA"  # Şehir
-    District_to_search = "ANKARA"  # İlçe
+    country = "TURKEY"  # Ülke
+    province = "ANKARA"  # Şehir
+    district = "ANKARA"  # İlçe
+    url = "https://ezanvakti.herokuapp.com"
 
     # Find country ID
-    countries = requests.get("http://ezanvakti.herokuapp.com/ulkeler")
+    countries = requests.get(url + "/ulkeler")
     for data_country in countries.json():
-        if data_country["UlkeAdiEn"] == Country_to_search.upper():
+        if data_country["UlkeAdiEn"] == country.upper():
             country_id = data_country["UlkeID"]
 
     # Find province ID
-    url_province = "http://ezanvakti.herokuapp.com/sehirler?ulke=" + country_id
-    provinces = requests.get(url_province)
+    provinces = requests.get(url + f"/sehirler?ulke={country_id}")
     for data_province in provinces.json():
-        if data_province["SehirAdiEn"] == Province_to_search.upper():
+        if data_province["SehirAdiEn"] == province.upper():
             province_id = data_province["SehirID"]
 
     # Find district ID
-    url_district = "http://ezanvakti.herokuapp.com/ilceler?sehir=" + province_id
-    districts = requests.get(url_district)
+    districts = requests.get(url + f"/ilceler?sehir={province_id}")
     for data_district in districts.json():
-        if data_district["IlceAdiEn"] == District_to_search.upper():
+        if data_district["IlceAdiEn"] == district.upper():
             district_id = data_district["IlceID"]
 
     # Get prayer times for the district
-    url_prayer = "http://ezanvakti.herokuapp.com/vakitler?ilce=" + district_id
-    prayer = requests.get(url_prayer)
+    prayer = requests.get(url + f"/vakitler?ilce={district_id}")
     return prayer.json()
 
 
-<<<<<<< HEAD
-def write_to_file(filename, data):
-    file = open(filename + ".ezan", mode='w', encoding='utf-8')
-    file.write(json.dumps(data))
-    file.close()
-    return
+def write_to_file(data):
+    with open('.ptimes.json', 'w') as json_file:
+        json.dump(data, json_file)
+
 
 def read_file(filename):
-    file = open(filename + ".ezan", mode='r', encoding='utf-8')
-    data = json.loads(file.read())
+    with open(filename, mode='r', encoding='utf-8') as json_file:
+        data = json.loads(json_file.read())
     present_day = datetime.datetime.now()
-    present_day = present_day.strftime('%d.%m.%Y')
-    print(data)
     for day in data:
-        if day["MiladiTarihKisa"] == present_day:
+        if day["MiladiTarihKisa"] == present_day.strftime('%d.%m.%Y'):
             print(day["Aksam"])
+            maghrib = datetime.datetime.strptime(day["Aksam"], '%H:%M')
+            sunrise = datetime.datetime.strptime(day["Gunes"], '%H:%M')
+            asr = datetime.datetime.strptime(day["Ikindi"], '%H:%M')
+            fajr = datetime.datetime.strptime(day["Imsak"], '%H:%M')
+            dhuhr = datetime.datetime.strptime(day["Ogle"], '%H:%M')
+            isha = datetime.datetime.strptime(day["Yatsi"], '%H:%M')
+            print(isha - present_day)
 
-=======
-def write_to_file(data):
-    file = open("test.ezan", mode='w', encoding='utf-8')
-    for element in data:
-        file.write(json.dumps(element) + "/n")
-    file.close()
-    return
->>>>>>> parent of 619c7a0... minor performance changes and code quality improvements
 
-#write_to_file("testfile01", get_prayer_times())
-read_file("testfile01")
+# write_to_file(get_prayer_times())
+read_file('.ptimes.json')
 
