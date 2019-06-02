9bceae6158568b178ee397eaae8b828c198d5da8
AYGIT1 <AYGIT1@users.noreply.github.com>
2019-06-02 07:34:36 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -40,22 +40,46 @@ def write_to_file(data):
         json.dump(data, json_file)
 
 
-def read_file(filename):
+def convert_datetime(filename):
     with open(filename, mode='r', encoding='utf-8') as json_file:
         data = json.loads(json_file.read())
-    present_day = datetime.datetime.now()
-    for day in data:
-        if day["MiladiTarihKisa"] == present_day.strftime('%d.%m.%Y'):
-            print(day["Aksam"])
-            maghrib = datetime.datetime.strptime(day["Aksam"], '%H:%M')
-            sunrise = datetime.datetime.strptime(day["Gunes"], '%H:%M')
-            asr = datetime.datetime.strptime(day["Ikindi"], '%H:%M')
-            fajr = datetime.datetime.strptime(day["Imsak"], '%H:%M')
-            dhuhr = datetime.datetime.strptime(day["Ogle"], '%H:%M')
-            isha = datetime.datetime.strptime(day["Yatsi"], '%H:%M')
-            print(isha - present_day)
+    present_time = datetime.datetime.now()
+    for ptime in data:
+        if ptime["MiladiTarihKisa"] == present_time.strftime('%d.%m.%Y'):
+            maghrib = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Aksam"], '%d.%m.%Y%H:%M')
+            sunrise = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Gunes"], '%d.%m.%Y%H:%M')
+            asr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Ikindi"], '%d.%m.%Y%H:%M')
+            fajr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Imsak"], '%d.%m.%Y%H:%M')
+            dhuhr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Ogle"], '%d.%m.%Y%H:%M')
+            isha = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Yatsi"], '%d.%m.%Y%H:%M')
+
+            if present_time < fajr:
+                print("Fajr:", datetime.datetime.strftime(fajr, '%H:%M'))
+                print("Remaining time until fajr:", fajr - present_time)
+            elif present_time < sunrise:
+                print("Sunrise:", datetime.datetime.strftime(sunrise, '%H:%M'))
+                print("Remaining time until sunrise:", sunrise - present_time)
+            elif present_time < dhuhr:
+                print("Dhuhr:", datetime.datetime.strftime(dhuhr, '%H:%M'))
+                print("Remaining time until dhuhr:", dhuhr - present_time)
+            elif present_time < asr:
+                print("Asr:", datetime.datetime.strftime(asr, '%H:%M'))
+                print("Remaining time until asr:", asr - present_time)
+            elif present_time < maghrib:
+                print("Maghrib:", datetime.datetime.strftime(maghrib, '%H:%M'))
+                print("Remaining time until maghrib:", maghrib - present_time)
+            elif present_time < isha:
+                print("Isha:", datetime.datetime.strftime(isha, '%H:%M'))
+                print("Remaining time until isha:", isha - present_time)
+            else:
+                print("no")
+            return
+
+    print("Cache is outdated. Updating...")
+    write_to_file(get_prayer_times())
+    convert_datetime('.ptimes.json')
 
 
 # write_to_file(get_prayer_times())
-read_file('.ptimes.json')
+convert_datetime('.ptimes.json')
 
