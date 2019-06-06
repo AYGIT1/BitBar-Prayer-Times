98b96aba5a433e375613495377b429c2703abb4f
AYGIT1 <AYGIT1@users.noreply.github.com>
2019-06-06 21:17:06 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -47,85 +47,62 @@ def get_prayer_times():
     return prayer.json()
 
 
+# Dump JSON formatted data to file
 def write_to_file(data):
     with open('.ptimes.json', 'w') as json_file:
         json.dump(data, json_file)
 
 
+def rerun(error_text):
+    errprint(error_text)
+    write_to_file(get_prayer_times())
+    convert_datetime('.ptimes.json')
+
+
+# Convert JSON formatted file to datetime object
 def convert_datetime(filename):
+    data = []
     try:
         with open(filename, mode='r', encoding='utf-8') as json_file:
             try:
                 data = json.loads(json_file.read())
             except json.decoder.JSONDecodeError:
-                errprint("Cache file corrupted, downloading file...")
-                write_to_file(get_prayer_times())
-                convert_datetime('.ptimes.json')
+                rerun("Cache file corrupted, downloading file...")
     except FileNotFoundError:
-        print("Cache file not found, downloading file...")
-        write_to_file(get_prayer_times())
-        convert_datetime('.ptimes.json')
-
+        rerun("Cache file not found, downloading file...")
     present_time = datetime.datetime.now()
     present_time = present_time - datetime.timedelta(microseconds=present_time.microsecond)
     for ptime in data:
-        next_date = present_time + datetime.timedelta(days=1)
-        if ptime["MiladiTarihKisa"] == present_time.strftime('%d.%m.%Y'):
-            maghrib = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Aksam"], '%d.%m.%Y%H:%M')
-            sunrise = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Gunes"], '%d.%m.%Y%H:%M')
-            asr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Ikindi"], '%d.%m.%Y%H:%M')
-            fajr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Imsak"], '%d.%m.%Y%H:%M')
-            dhuhr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Ogle"], '%d.%m.%Y%H:%M')
-            isha = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Yatsi"], '%d.%m.%Y%H:%M')
-
-        if ptime["MiladiTarihKisa"] == next_date.strftime('%d.%m.%Y'):
-            fajr_next = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Imsak"], '%d.%m.%Y%H:%M')
-            # TODO: The statements below could be shortened.
-            if present_time < fajr:
-                remtime = str(fajr - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-            elif present_time < sunrise:
-                remtime = str(sunrise - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-            elif present_time < dhuhr:
-                remtime = str(dhuhr - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-            elif present_time < asr:
-                remtime = str(asr - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-            elif present_time < maghrib:
-                remtime = str(maghrib - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-            elif present_time < isha:
-                remtime = str(isha - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-            elif present_time < fajr_next:
-                remtime = str(fajr_next - present_time)
-                remtime = ':'.join(remtime.split(':')[0:2])
-                print(remtime)
-
-            print('\n' '---' + '\n')
-            print( 'Fajr:\t', datetime.datetime.strftime(fajr, '%H:%M'), '| font = Menlo size = 12' + '\n')
-            print( 'Sunrise:\t', datetime.datetime.strftime(sunrise, '%H:%M'), '| font = Menlo size = 12' + '\n')
-            print( 'Dhuhr:\t', datetime.datetime.strftime(dhuhr, '%H:%M'), '| font = Menlo size = 12' + '\n')
-            print( 'Asr:\t', datetime.datetime.strftime(asr, '%H:%M'), '| font = Menlo size = 12' + '\n')
-            print( 'Maghrib:\t', datetime.datetime.strftime(maghrib, '%H:%M'), '| font = Menlo size = 12' + '\n')
-            print( 'Isha:\t', datetime.datetime.strftime(isha, '%H:%M'), '| font = Menlo size = 12' + '\n')
-            return
-
-    # print("no")
-    # print(sunrise)
-
-    # errprint("Cache is outdated, updating...")
-    # write_to_file(get_prayer_times())
-    # convert_datetime('.ptimes.json')
+        try:
+            if ptime["MiladiTarihKisa"] == present_time.strftime('%d.%m.%Y'):
+                index_nextday = data.index(ptime) + 1
+                maghrib = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Aksam"], '%d.%m.%Y%H:%M')
+                sunrise = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Gunes"], '%d.%m.%Y%H:%M')
+                asr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Ikindi"], '%d.%m.%Y%H:%M')
+                fajr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Imsak"], '%d.%m.%Y%H:%M')
+                dhuhr = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Ogle"], '%d.%m.%Y%H:%M')
+                isha = datetime.datetime.strptime(ptime["MiladiTarihKisa"] + ptime["Yatsi"], '%d.%m.%Y%H:%M')
+                try:
+                    fajr_next = datetime.datetime.strptime(
+                        data[index_nextday]["MiladiTarihKisa"] + data[index_nextday]["Imsak"], '%d.%m.%Y%H:%M')
+                except IndexError:
+                    rerun("Cache is outdated, updating...")
+                ptimeslist = [fajr, sunrise, dhuhr, asr, maghrib, isha, fajr_next]
+                pnameslist = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
+                for ptime in ptimeslist:
+                    if present_time < ptime:
+                        remtime = str(ptime - present_time)
+                        remtime = ':'.join(remtime.split(':')[0:2])
+                        print(remtime)
+                        print('\n' '---' + '\n')
+                        for pname in pnameslist:
+                            print(pname + ':\t', datetime.datetime.strftime(ptime, '%H:%M'),
+                                  '| font = Menlo size = 12' + '\n')
+                        return
+        except KeyError:
+            rerun("Unidentified JSON file, downloading file from server: https://ezanvakti.herokuapp.com  ...")
+        else:
+            rerun("Cache is outdated, updating...")
 
 
-# write_to_file(get_prayer_times())
 convert_datetime('.ptimes.json')
